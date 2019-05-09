cec134244e755a915b9218be6232838909b1a69a
christiancldon <50159127+christiancldon@users.noreply.github.com>
2019-05-08 22:37:06 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -22,4 +22,9 @@ $app->get('/', function() use($app) {
   return $app['twig']->render('index.twig');
 });
 
+$app->get('/alert', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('alert.twig');
+});
+
 $app->run();
