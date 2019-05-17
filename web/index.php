f0701d7893f35005aaad734fbad604ad5dccd138
christiancldon <50159127+christiancldon@users.noreply.github.com>
2019-05-16 19:27:20 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -27,24 +27,16 @@ $app->get('/monitor', function() use($app) {
   return $app['twig']->render('monitor.twig');
 });
 
-$app->get('/almacenar/{consumo}/{temperatura}/{apertura}', function() use($app) {
+$app->get('/almacenar/{consumo}/{temperatura}', function($consumo, $temperatura) use($app) {
   $app['monolog']->addDebug('logging output.');
 
-  $dbconexion=pg_connect( "host=ec2-23-21-192-179.compute-1.amazonaws.com port=5432 dbname=d83082i66m502p user=auwnpcmayyfvrf password=9c4d61ed2d9d293b80902dbfe5611d7bde523f1ec69d9853b8e349df73b2161c");
+  $dbconexion=pg_connect( "host=ec2-54-221-198-156.compute-1.amazonaws.com port=5432 dbname=daclr0pc5h658k user=kzlmednsrgkxpn password=e98f8854ed2ce71f7f34dae87b70a63675e41c8e6ba689aae13927da6a974232");
   $registro=array(
     "fecha"=>date('Y-m-d H:i:s'),
-    "voltaje_1"=>$v1,
-    "voltaje_2"=>$v2,
-      "corriente_1"=>$i1,
-        "corriente_2"=>$i2,
-        "corriente_3"=>$i3,
-        "potencia_1"=>$p1,
-        "potencia_2"=>$p2);
-  $resultado=pg_insert ($dbconexion,"medicion_test",$registro);
+    "temperatura"=>$temperatura,
+    "consumo"=>$consumo);
+  $resultado=pg_insert ($dbconexion,"monitor",$registro);
   return $resultado;
-
-  return "ok";
-
 });
 
 $app->run();
