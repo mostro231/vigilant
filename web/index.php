942650bcf8cd45fc83910e0a58e233fc4ceb102f
heyderpaez <heyderpaez@gmail.com>
2019-05-28 20:06:26 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -57,9 +57,8 @@ $app->get('/getTemperaturaData/{numberOfRecords}', function($numberOfRecords) us
   $app['monolog']->addDebug('logging output.');
 
   $dbconexion=pg_connect( "host=ec2-54-221-198-156.compute-1.amazonaws.com port=5432 dbname=daclr0pc5h658k user=kzlmednsrgkxpn password=e98f8854ed2ce71f7f34dae87b70a63675e41c8e6ba689aae13927da6a974232");
+  $consult_db = pg_query($dbconexion, 'SELECT "fecha","temperatura" FROM public."monitor" ORDER BY "fecha" DESC LIMIT ' . $numberOfRecords .'');
 
-
-  return $consult_db;
 
   $resultArray = array();
   while ($row = pg_fetch_row($consult_db, null, PGSQL_ASSOC)) {
